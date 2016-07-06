6c89adcafe797b58df935881103f1f5e4aae4c82
Harukao <mk2@mk2.red>
2016-07-06 11:05:04 +0900

    部分的にhyaliteに置き換える

--- app/application.rb
+++ app/application.rb
@@ -9,9 +9,23 @@ class Application
   end
 
   def render
-    div(nil,
-      div
-    )
+    Hyalite.create_element('container', nil,
+      Hyalite.create_element('header', {id: 'header', className: 'alt'},
+        h3(nil,
+          a({href: '/'}, 'NIJIBOX Engineer Blog'))),
+      Hyalite.create_element('section', {id: 'banner'},
+        i({className: 'icon'}),
+        h3(nil, '俺が魂を込めれば')),
+      Hyalite.create_element('section', {id: 'post', className: 'wrapper style3 special'},
+        div({className: 'inner'},
+          Hyalite.create_element('header', {className: 'major narrow'},
+            h2(nil, 'We Are Hiring! 積極採用中'),
+            p(nil, 'NIJIBOXの組織文化や働きやすい制度、技術的な背景など表から裏まで紙面の許す限り更に詳しく紹介します')),
+          ul({className: 'actions'},
+            li(nil,
+              a({href: 'http://nijibox-recruit.jp/engineer', className: 'button big alt'}, '詳しくはこちら')))
+          ))
+      )
   end
 
 end
